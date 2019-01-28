746ea8a3fc0f9e11996e58647e3089458782fc19
Cao Qi <caoqi@CaodeMacBook-Pro.local>
2019-01-28 13:48:43 +0800

    second modification

--- bed6Tobed12.py
+++ bed6Tobed12.py
@@ -2,8 +2,9 @@
 """
 @aim: transform bed6 to bed12 according to the fourth column.
 	blocks with the same fourth column couldn't overlap!
-@usage: bed6Tobed12.py file.bed(6) > file_new.bed(12)
-@author: Miki Cao
+@usage: python3 bed6Tobed12.py file.bed(6) > file_new.bed(12)
+=============================================================
+@author: ustbcaoqi (Miki Cao)
 @date: Jan 24, 2019
 """
 
@@ -32,44 +33,43 @@ for bed6_line in bed6:
 
 # for each gene, print one line, caontaining block informations.
 for gene in sorted(my_dict.keys()):
-	exons = my_dict[gene]
+	blocks = my_dict[gene]
 
-	# split each block(exon)
-	exon_start = []
-	exon_end = []
-	for exon in exons:
-		exon_features = exon.split()
-		chrom = exon_features[0]
-		gene_id = exon_features[3]
-		strand = exon_features[5]
-		exon_start.append( int(exon_features[1]) )
-		exon_end.append( int(exon_features[2]) )
+	# split each block(block)
+	block_start = []
+	block_end = []
+	for block in blocks:
+		block_features = block.split()
+		chrom = block_features[0]
+		gene_id = block_features[3]
+		strand = block_features[5]
+		block_start.append( int(block_features[1]) )
+		block_end.append( int(block_features[2]) )
 
-	# sort exon positions, exons couldn't overlap!!!!
-	exon_start.sort()
-	exon_end.sort()
+	# sort block positions, blocks couldn't overlap!!!!
+	block_start.sort()
+	block_end.sort()
 
-	# calculate length and relative start each exon
-	exon_len = []
-	exon_rela_start = []
-	for i in range(len(exon_start)):
-		exon_len.append( exon_end[i] - exon_start[i] )
-		exon_rela_start.append( exon_start[i] - exon_start[0] )
+	# calculate length and relative start each block
+	block_len = []
+	block_rela_start = []
+	for i in range(len(block_start)):
+		block_len.append( block_end[i] - block_start[i] )
+		block_rela_start.append( block_start[i] - block_start[0] )
 
+	blocks_num = len(block_len)
 	# print standard BED12 format
-	print(chrom, exon_start[0], exon_end[-1], gene_id, '0', strand,
-		exon_start[0], exon_end[-1], '255,0,0', len(exon_start), sep='\t', end='\t')
+	print(chrom, block_start[0], block_end[-1], gene_id, '0', strand,
+		block_start[0], block_end[-1], '255,0,0', blocks_num, sep='\t', end='\t')
 
-	for i in range(len(exon_len)):
-		print(exon_len[i], end='')
-		if (i+1) != len(exon_len):
-			print(',',end='')
+	for i in range(blocks_num):
+		print(block_len[i], end=',')
 
 	print("\t", end='')
 
-	for i in range(len(exon_rela_start)):
-		print(exon_rela_start[i], end='')
-		if (i+1) != len(exon_rela_start):
-			print(',',end='')
+	for i in range(blocks_num):
+		print(block_rela_start[i], end=',')
 
 	print('')
+
+
